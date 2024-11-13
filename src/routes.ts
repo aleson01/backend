66c2bafe6b7ef862843fbeab31313fe05728acc1
aleson01 <alesonsanley@gmail.com>
2024-11-13 19:55:57 -0400

    feat: Adicionar Categorias

--- src/routes.ts
+++ src/routes.ts
@@ -3,12 +3,16 @@ import CreateUserController from './controllers/user/CreateUserController';
 import ListUserController from './controllers/user/ListaUserController';
 import { AuthUserController } from './controllers/user/AuthUserController';
 import { DetailuserController } from './controllers/user/DetailUserController';
+import {isAuthenticated} from './middlewares/isAuthenticated'
+import {CreateCategoryController } from './controllers/category/CreateCategoryController'
 
 const router = express.Router();
 
-router.post('/usuario', async(req:Request, res:Response)=>{ return new CreateUserController(). handle(req, res); });
-router.get('/usuario', async(req:Request, res:Response)=>{ return new ListUserController(). handle(req, res); });
+router.post('/usuario', async(req:Request, res:Response)=>{ return new CreateUserController().handle(req, res); });
+router.get('/usuario', async(req:Request, res:Response)=>{ return new ListUserController().handle(req, res); });
 router.post('/session', async(req:Request, res:Response)=>{ return new AuthUserController().handle(req, res); });
-router.get('/me', async(req:Request, res:Response)=>{ return new DetailuserController().handle(req, res); });
+router.get('/me', isAuthenticated, async(req:Request, res:Response)=>{ return new DetailuserController().handle(req, res); });
+router.post('/category', isAuthenticated, async(req:Request, res:Response)=>{ return new CreateCategoryController().handle(req, res); });
+
 
 export default router;
