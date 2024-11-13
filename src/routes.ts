78036342d78d784b6ef19c5bec4aceefc2c6b19d
aleson01 <81500671+aleson01@users.noreply.github.com>
2024-11-13 12:00:57 -0400

    feat: Autenticar login de usuario

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,12 @@
 import express, {  Request, Response } from 'express';
 import CreateUserController from './controllers/user/CreateUserController';
 import ListUserController from './controllers/user/ListaUserController';
+import { AuthUserController } from './controllers/user/AuthUserController';
 
 const router = express.Router();
 
 router.post('/usuario', async(req:Request, res:Response)=>{ return new CreateUserController(). handle(req, res); });
 router.get('/usuario', async(req:Request, res:Response)=>{ return new ListUserController(). handle(req, res); });
+router.post('/session', async(req:Request, res:Response)=>{ return new AuthUserController().handle(req, res); });
 
 export default router;
---
+++ src/services/user/AuthUserService.ts
@@ -0,0 +1,54 @@
+import prismaClient from "../../prisma";
+import { compare } from 'bcryptjs'
+import { sign } from 'jsonwebtoken'
+
+interface AuthRequest{
+  email: string;
+  password: string;
+}
+
+class AuthUserService{
+  async execute({ email, password }: AuthRequest){
+    //Verificar se o email existe.
+    const user = await prismaClient.user.findFirst({
+      where:{
+        email: email
+      }
+    })
+
+    if(!user){
+      throw new Error("Email ou Senha informado está incorreto")
+    }
+
+    // preciso verificar se a senha que ele mandou está correta.
+    const passwordHash = await compare(password, user.password)
+
+    if(!passwordHash){
+      throw new Error("Email ou Senha informado está incorreto")
+    }
+
+
+    // Se deu tudo certo vamos gerar o token pro usuario.
+    const token = sign(
+      {
+        name: user.name,
+        email: user.email
+      },
+      process.env.SECRET_KEY,
+      {
+        subject: user.id,
+        expiresIn: '30d'
+      }
+    )
+
+
+    return { 
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      token: token
+     }
+  }
+}
+
+export { AuthUserService };
