253cf28ad2fcf74ad20a01aa3bc8f1743da82c91
aleson01 <81500671+aleson01@users.noreply.github.com>
2024-11-13 12:14:13 -0400

    feat: Adicionar Middleware

--- src/routes.ts
+++ src/routes.ts
@@ -2,11 +2,13 @@ import express, {  Request, Response } from 'express';
 import CreateUserController from './controllers/user/CreateUserController';
 import ListUserController from './controllers/user/ListaUserController';
 import { AuthUserController } from './controllers/user/AuthUserController';
+import { DetailuserController } from './controllers/user/DetailUserController';
 
 const router = express.Router();
 
 router.post('/usuario', async(req:Request, res:Response)=>{ return new CreateUserController(). handle(req, res); });
 router.get('/usuario', async(req:Request, res:Response)=>{ return new ListUserController(). handle(req, res); });
 router.post('/session', async(req:Request, res:Response)=>{ return new AuthUserController().handle(req, res); });
+router.get('/me', async(req:Request, res:Response)=>{ return new DetailuserController().handle(req, res); });
 
 export default router;
