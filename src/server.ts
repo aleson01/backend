6b4489b4e639909cbffb28b3c7ff3fcfea9d5d8e
aleson01 <alesonsanley@gmail.com>
2024-11-13 21:28:36 -0400

    fix: Cadastrar produto enviando imagem

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import router from './routes';
+import path from 'path';
 
 
 const app = express();
@@ -8,6 +9,7 @@ app.use(express.json());
 
 app.use('/auth', router);
 
+app.use('/files', express.static(path.resolve(__dirname,'..','tmp')))
 
 const PORT = process.env.PORT || 3000;
 
